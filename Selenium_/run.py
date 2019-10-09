024f207d490e774cf5d892060a8f33ba4f6aba73
Fuhp <fuhp@rightknights.com>
2019-10-09 11:22:02 +0800

    '3'

--- Selenium_/run.py
+++ Selenium_/run.py
@@ -5,6 +5,6 @@ from Chrom import MyChrom
 if __name__ == '__main__':
     chrom = MyChrom()
     url = 'https://www.zhihu.com/signup?next=%2F'
-    cookie = chrom.get_noheadless_cookie(url, 45,)
+    cookie = chrom.get_noheadless_cookie(url, 35,)
     chrom.use_cookie(url, )
     # this is test
